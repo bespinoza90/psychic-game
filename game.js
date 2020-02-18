38e1d426eb7d1b7975df801b78377ffd45929a91
Bobby Espinoza <bobbyespinoza@gmail.com>
2020-02-18 12:17:38 -0700

    added javascript

---
+++ game.js
@@ -0,0 +1,15 @@
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var guessedLetters = [];
+
+var win = 0;
+
+var lose = 0;
+
+var guesses = 10;
+
+var winText = document.getElementById("win-text");
+var loseText = document.getElementById("lose-text");
+var guessText = document.getElementById("guess-text");
+var guessLeft = document.getElementById("guess-left");
