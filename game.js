aff7f15ecbebe1fd9df2d373372f9595a62bc437
Bobby Espinoza <bobbyespinoza@gmail.com>
2020-02-18 15:03:41 -0700

    added more javascript and some css

--- game.js
+++ game.js
@@ -13,3 +13,50 @@ var winText = document.getElementById("win-text");
 var loseText = document.getElementById("lose-text");
 var guessText = document.getElementById("guess-text");
 var guessLeft = document.getElementById("guess-left");
+
+
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log(computerGuess);
+
+
+var reset = function () {
+    guesses = 10;
+    guessedLetters = [];
+    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerGuess);
+}
+
+
+document.onkeyup = function (event) {
+    var userGuess = event.key.toUpperCase();
+    console.log(userGuess);
+
+    if (guesses === 0) {
+        alert("The computer guess was " + computerGuess + " try again!!!");
+        lose++;
+        reset();
+    }
+
+
+    else if (computerGuess === userGuess) {
+        win++;
+        guess = 10;
+        guessedLetters.push(userGuess);
+        alert("Good Job! You guessed correct");
+        computerGuess = letters[Math.floor(Math.random() * letters.length)];
+        reset();
+    }
+
+
+    else if (computerGuess !== userGuess) {
+        guesses--;
+        guessedLetters.push(userGuess);
+        alert("Guess again!");
+    }
+
+
+    winText.textContent = "Wins: " + win;
+    loseText.textContent = "Losses: " + lose;
+    guessText.textContent = "Guesses: " + guesses;
+    guessLeft.textContent = guessedLetters;
+};
